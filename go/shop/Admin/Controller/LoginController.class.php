a53da1970a4eb9810d8e2d5b2c4912a40a45bde3
Administrator <Administrator@USER-20150409MT>
2016-01-29 10:29:56 +0800

    1、增加了刷新验证码的功能

--- go/shop/Admin/Controller/LoginController.class.php
+++ go/shop/Admin/Controller/LoginController.class.php
@@ -13,33 +13,33 @@ class LoginController extends AdminController {
             $where = array();
             $where['mg_name'] = $data['mg_name'];
             $result = $Login->where($where)->field('mg_id,mg_name,mg_pwd,mg_time,mg_role_id')->find();
-
-            // 验证用户名 对比 密码
-            if ($result && $data['mg_pwd'] == $result['mg_pwd']) {
-                // 存储session
-                session('mg_id', $result['mg_id']);          // 当前用户id
-                session('mg_name', $result['mg_name']);   // 当前用户名
-                $verify = I('param.verify','');  
-                //if (check_verify($_POST['captcha'])) {
-                if (check_verify($verify)){
-                    $this->success('登录成功,正跳转至系统首页...', U('Index/index'), 3);
+            $ver = new \Think\Verify();
+            if ($ver -> check($_POST['captcha'])) {
+                // 验证用户名 对比 密码
+                if ($result && $data['mg_pwd'] == $result['mg_pwd']) {
+                    // 存储session
+                    session('mg_id', $result['mg_id']);          // 当前用户id
+                    session('mg_name', $result['mg_name']);   // 当前用户名
+                    //$this->success('登录成功,正跳转至系统首页...', U('Index/index'), 3);
+                    $this->redirect('Index/index', '登录成功,正跳转至系统首页...');
                 } else {
-                     $this->error('亲，验证码输错了哦！');
+                    $this->error('登录失败,用户名或密码不正确!');
                 }
             } else {
-                $this->error('登录失败,用户名或密码不正确!');
+                $this->error('亲，验证码输入错误!');
             }
         } else {
             $this->display();
         }
     }
 
-    Public function verify() {
+    public function verify() {
+        ob_clean();
         $Verify = new \Think\Verify();
         $Verify->fontSize = 9;
         $Verify->length = 4;
         $Verify->useNoise = false;
-        $Verify->codeSet = '0123456789';  
+        $Verify->codeSet = '0123456789';
         $Verify->entry();
     }
 
